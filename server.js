f29df32d458dc8bccb098e37ca7d288886b9a715
Rogerio rodrigues <rogerio25123@hotmail.com>
2021-07-11 19:19:59 -0400

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/mymoney-ui'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/dist/mymoney-ui/index.html');
 });
 
 app.listen(process.env.PORT || 4200 );
