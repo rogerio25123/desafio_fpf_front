ef26379e6327e4ab3e1cc1a65a0ea5ed9bf44dda
Rogerio rodrigues <rogerio25123@hotmail.com>
2021-07-11 11:05:45 -0400

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://back-desafio-rogerio.herokuapp.com',
 
-  tokenWhitelistedDomains: [ new RegExp('back-desafio-rogeri.herokuapp.com') ],
+  tokenWhitelistedDomains: [ new RegExp('back-desafio-rogerio.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
